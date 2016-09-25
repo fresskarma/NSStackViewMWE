a9b85b91957eacc1bc7e52e90c96ab3807270d69
Lucas Jenss <lucas@x3ro.de>
2016-09-25 22:31:01 +0200

    Initial Commit

---
+++ NSStackviewQuestion/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NSStackviewQuestion
+//
+//  Created by Lucas on 25/09/16.
+//  Copyright © 2016 foobar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
